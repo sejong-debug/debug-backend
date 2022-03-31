b0d8ab7cb3e30660dd1064049a92e014e75218bf
Cho-D-YoungRae <yrc9229@gmail.com>
2022-03-31 21:38:21 +0900

    DELETE: 불필요한 hello 패키지 제거

--- src/main/java/org/sj/capston/debug/debugbackend/hello/HelloRepository.java
+++
@@ -1,6 +0,0 @@
-package org.sj.capston.debug.debugbackend.hello;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HelloRepository extends JpaRepository<Hello, Long> {
-}
