f0f40bc1db023fc82c38fb934eaace1344bca347
Cho-D-YoungRae <yrc9229@gmail.com>
2022-04-01 13:20:01 +0900

    FEAT: JoinDto validation 추가

--- src/main/java/org/sj/capston/debug/debugbackend/dto/JoinDto.java
+++ src/main/java/org/sj/capston/debug/debugbackend/dto/JoinDto.java
@@ -1,13 +1,21 @@
 package org.sj.capston.debug.debugbackend.dto;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 public class JoinDto {
 
+    @NotEmpty
+    @Size(max = 45)
     private String username;
 
+    @NotEmpty
     private String password;
 
+    @NotEmpty
     private String name;
 }
