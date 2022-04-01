a41d5d1e6fb7953f3fe74ef4fdd5d69f1b7161f2
Cho-D-YoungRae <yrc9229@gmail.com>
2022-04-01 16:05:29 +0900

    DELETE: ProjectCrop 관련 제거

--- src/main/java/org/sj/capston/debug/debugbackend/repository/ProjectCropRepository.java
+++
@@ -1,7 +0,0 @@
-package org.sj.capston.debug.debugbackend.repository;
-
-import org.sj.capston.debug.debugbackend.entity.ProjectCrop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectCropRepository extends JpaRepository<ProjectCrop, Long> {
-}
