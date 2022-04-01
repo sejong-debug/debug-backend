716c721552daef515802e6a1d76cb3ee1b12699c
Cho-D-YoungRae <yrc9229@gmail.com>
2022-04-01 13:19:39 +0900

    FEAT: LoginDto validation 추가

--- src/main/java/org/sj/capston/debug/debugbackend/dto/LoginDto.java
+++ src/main/java/org/sj/capston/debug/debugbackend/dto/LoginDto.java
@@ -2,10 +2,14 @@ package org.sj.capston.debug.debugbackend.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class LoginDto {
 
+    @NotEmpty
     private String username;
 
+    @NotEmpty
     private String password;
 }
