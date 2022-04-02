cebc400e4f4445f4571b9e19a1323921a6252e0e
Cho-D-YoungRae <yrc9229@gmail.com>
2022-04-03 02:16:54 +0900

    REFACTOR: getAllCropTypes() IndexController -> ProjectController

--- src/main/java/org/sj/capston/debug/debugbackend/controller/IndexController.java
+++ src/main/java/org/sj/capston/debug/debugbackend/controller/IndexController.java
@@ -3,13 +3,20 @@ package org.sj.capston.debug.debugbackend.controller;
 import lombok.RequiredArgsConstructor;
 import org.sj.capston.debug.debugbackend.auth.PrincipalDetails;
 import org.sj.capston.debug.debugbackend.dto.JoinDto;
+import org.sj.capston.debug.debugbackend.entity.CropType;
 import org.sj.capston.debug.debugbackend.service.MemberService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -18,13 +25,15 @@ public class IndexController {
     private final MemberService memberService;
 
     @PostMapping("/join")
-    public ResponseEntity<Void> join(@RequestBody JoinDto joinDto) {
+    public ResponseEntity<Void> join(@Validated @RequestBody JoinDto joinDto) {
         memberService.join(joinDto);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity
+                .created(linkTo(IndexController.class).toUri())
+                .build();
     }
 
     @GetMapping("/")
     public ResponseEntity<Void> index(@AuthenticationPrincipal PrincipalDetails principalDetails) {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }
