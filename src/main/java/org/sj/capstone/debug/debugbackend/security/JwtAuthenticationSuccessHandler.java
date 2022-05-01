b6a838b88c20ec4a0db6f65971f2d635c34fd9bc
Cho-D-YoungRae <yrc9229@gmail.com>
2022-05-02 00:03:34 +0900

    FEAT: 로그인 성공 API 응답 수정

--- src/main/java/org/sj/capstone/debug/debugbackend/security/JwtAuthenticationSuccessHandler.java
+++ src/main/java/org/sj/capstone/debug/debugbackend/security/JwtAuthenticationSuccessHandler.java
@@ -2,6 +2,7 @@ package org.sj.capstone.debug.debugbackend.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.sj.capstone.debug.debugbackend.dto.common.ApiResult;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -25,9 +26,12 @@ public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHan
         String accessToken = jwtTokenProvider.createToken(authentication);
         JwtResponseDto jwtResponseDto = new JwtResponseDto();
         jwtResponseDto.setAccessToken(accessToken);
-        
+        ApiResult<JwtResponseDto> result = ApiResult.<JwtResponseDto>builder()
+                .data(jwtResponseDto)
+                .build();
+
         response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        objectMapper.writeValue(response.getWriter(), jwtResponseDto);
+        objectMapper.writeValue(response.getWriter(), result);
     }
 }
