34f8cce28169f62200b17c9720a3fc816e76c405
Cho-D-YoungRae <yrc9229@gmail.com>
2022-05-02 23:24:37 +0900

    TEST: 회원가입 성공 테스트

--- src/test/java/org/sj/capstone/debug/debugbackend/controller/MemberControllerTest.java
+++ src/test/java/org/sj/capstone/debug/debugbackend/controller/MemberControllerTest.java
@@ -4,29 +4,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.sj.capstone.debug.debugbackend.common.RestDocsConfig;
 import org.sj.capstone.debug.debugbackend.dto.member.MemberJoinDto;
-import org.sj.capstone.debug.debugbackend.entity.Member;
-import org.sj.capstone.debug.debugbackend.repository.MemberRepository;
-import org.sj.capstone.debug.debugbackend.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.hateoas.MediaTypes;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.restdocs.headers.HeaderDocumentation.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -35,15 +27,41 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Import(RestDocsConfig.class)
 @ActiveProfiles("test")
 @Transactional
-class IndexControllerTest {
+class MemberControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
-    void index() throws Exception {
-        mockMvc.perform(get("/"))
+    void 회원가입_성공() throws Exception {
+        String username = "test-username";
+        String password = "test-password!";
+        String name = "test-name";
+
+        MemberJoinDto memberJoinDto = new MemberJoinDto();
+        memberJoinDto.setUsername(username);
+        memberJoinDto.setPassword(password);
+        memberJoinDto.setName(name);
+
+        mockMvc.perform(post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(memberJoinDto)))
                 .andDo(print())
-                .andExpect(status().isNoContent());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("success").value(true))
+                .andDo(document("join",
+                        requestFields(
+                                fieldWithPath("username").description("회원가입 ID"),
+                                fieldWithPath("password").description("회원가입 비밀번호"),
+                                fieldWithPath("name").description("회원가입 이름")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").description("성공 여부")
+                        )
+                ));
     }
 }
