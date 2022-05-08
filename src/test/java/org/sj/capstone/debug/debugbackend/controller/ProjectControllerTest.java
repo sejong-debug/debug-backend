7b3bf79efe80bdcea2b3e2a2e30d6f0bb9572b9e
Cho-D-YoungRae <yrc9229@gmail.com>
2022-05-08 13:04:43 +0900

    TEST: 프로젝트 생성

--- src/test/java/org/sj/capstone/debug/debugbackend/controller/ProjectControllerTest.java
+++ src/test/java/org/sj/capstone/debug/debugbackend/controller/ProjectControllerTest.java
@@ -1,9 +1,11 @@
 package org.sj.capstone.debug.debugbackend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.sj.capstone.debug.debugbackend.common.RestDocsConfig;
 import org.sj.capstone.debug.debugbackend.common.TestDataConfig;
+import org.sj.capstone.debug.debugbackend.dto.common.ApiResult;
 import org.sj.capstone.debug.debugbackend.dto.member.MemberJoinDto;
 import org.sj.capstone.debug.debugbackend.dto.project.ProjectCreationDto;
 import org.sj.capstone.debug.debugbackend.security.JwtTokenProvider;
@@ -31,9 +33,10 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -59,7 +62,8 @@ class ProjectControllerTest {
 
     @Test
     @WithUserDetails
-    void getAllCropTypesTest() throws Exception{
+    @DisplayName("프로젝트 작물 종류 조회")
+    void getAllCropTypesTest() throws Exception {
 
         mockMvc.perform(get("/projects/crop-types")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -69,8 +73,8 @@ class ProjectControllerTest {
                 .andExpect(jsonPath("success").value(true))
                 .andExpect(jsonPath("data").value(
                         Arrays.stream(CropType.values())
-                        .map(Enum::name)
-                        .collect(Collectors.toList())))
+                                .map(Enum::name)
+                                .collect(Collectors.toList())))
                 .andDo(document("get-crop-types",
                         responseFields(
                                 fieldWithPath("success").description("요청 성공 여부"),
@@ -79,21 +83,38 @@ class ProjectControllerTest {
                 ));
     }
 
-//    @Test
-//    void createProjectTest() throws Exception {
-//        String loginToken = JwtTokenProvider.TOKEN_PREFIX + joinAndLogin();
-//
-//        ProjectCreationDto creationDto = new ProjectCreationDto();
-//        creationDto.setName("test-project");
-//        creationDto.setStartDate(LocalDate.of(2022, 3, 1));
-//        creationDto.setEndDate(LocalDate.of(2022, 12, 1));
-//        creationDto.setCropType(CropType.팥);
-//
-//        mockMvc.perform(post("/projects")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .accept(MediaTypes.HAL_JSON)
-//                        .header(HttpHeaders.AUTHORIZATION, loginToken)
-//                        .content(objectMapper.writeValueAsString(creationDto)))
-//                .andDo(print());
-//    }
+    @Test
+    @WithUserDetails
+    @DisplayName("프로젝트 생성")
+    void createProjectTest() throws Exception {
+
+        ProjectCreationDto creationDto = new ProjectCreationDto();
+        creationDto.setName("테스트 프로젝트");
+        creationDto.setStartDate(LocalDate.of(2022, 3, 1));
+        creationDto.setEndDate(LocalDate.of(2022, 12, 1));
+        creationDto.setCropType(CropType.팥);
+
+        mockMvc.perform(post("/projects")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(creationDto)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("success").value(true))
+                .andDo(document("create-project",
+                        requestFields(
+                                fieldWithPath("name").description("프로젝트 이름"),
+                                fieldWithPath("cropType").description("프로젝트에서 관리할 작물 종류"),
+                                fieldWithPath("startDate").description("프로젝트 시작 날짜"),
+                                fieldWithPath("endDate").description("프로젝트 종료 예상 날짜"))
+                        ,
+                        responseHeaders(
+                                headerWithName(HttpHeaders.LOCATION).description("생성된 프로젝트 조회 요청")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").description("요청 성공 여부"),
+                                fieldWithPath("data").description("생성된 프로젝트 아이디")
+                        )
+                ));
+    }
 }
