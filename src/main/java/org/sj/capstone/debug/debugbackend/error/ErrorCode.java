f44975b4eb59ec6916a7a8e459184b21bbba22dc
Cho-D-YoungRae <yrc9229@gmail.com>
2022-05-11 11:52:46 +0900

    FEAT: NOT_OWNED_RESOURCE 에러 정의
    자신이 소유하지 않은 자원 요청에 대한 에러

--- src/main/java/org/sj/capstone/debug/debugbackend/error/ErrorCode.java
+++ src/main/java/org/sj/capstone/debug/debugbackend/error/ErrorCode.java
@@ -13,6 +13,7 @@ public enum ErrorCode {
     INVALID_INPUT_VALUE(COMMON, SC_BAD_REQUEST, " Invalid Input Value"),
     INVALID_TYPE_VALUE(COMMON, SC_BAD_REQUEST,  " Invalid Type Value"),
     RESOURCE_NOT_FOUND(COMMON, SC_BAD_REQUEST, " Resource is Not Found"),
+    NOT_OWNED_RESOURCE(COMMON, SC_FORBIDDEN, "Resource is not owned"),
 
     LOGIN_INPUT_INVALID(MEMBER, SC_BAD_REQUEST, "Login input is invalid"),
     USERNAME_DUPLICATION(MEMBER, SC_CONFLICT, "Username is Duplication"),
