5013c11e720c29c2ce1a3498d07c255b54037a19
Cho-D-YoungRae <yrc9229@gmail.com>
2022-05-11 11:54:40 +0900

    TEST: 프로젝트 조회 API

--- src/test/java/org/sj/capstone/debug/debugbackend/controller/ProjectControllerTest.java
+++ src/test/java/org/sj/capstone/debug/debugbackend/controller/ProjectControllerTest.java
@@ -5,39 +5,34 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.sj.capstone.debug.debugbackend.common.RestDocsConfig;
 import org.sj.capstone.debug.debugbackend.common.TestDataConfig;
-import org.sj.capstone.debug.debugbackend.dto.common.ApiResult;
-import org.sj.capstone.debug.debugbackend.dto.member.MemberJoinDto;
 import org.sj.capstone.debug.debugbackend.dto.project.ProjectCreationDto;
-import org.sj.capstone.debug.debugbackend.security.JwtTokenProvider;
-import org.sj.capstone.debug.debugbackend.dto.security.JwtResponseDto;
-import org.sj.capstone.debug.debugbackend.dto.security.LoginDto;
 import org.sj.capstone.debug.debugbackend.entity.CropType;
+import org.sj.capstone.debug.debugbackend.entity.Project;
+import org.sj.capstone.debug.debugbackend.repository.ProjectRepository;
 import org.sj.capstone.debug.debugbackend.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.print.attribute.standard.Media;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -59,6 +54,9 @@ class ProjectControllerTest {
 
     @Autowired
     MemberService memberService;
+
+    @Autowired
+    ProjectRepository projectRepository;
 
     @Test
     @WithUserDetails
@@ -114,6 +112,49 @@ class ProjectControllerTest {
                         responseFields(
                                 fieldWithPath("success").description("요청 성공 여부"),
                                 fieldWithPath("data").description("생성된 프로젝트 아이디")
+                        )
+                ));
+    }
+
+    @Test
+    @WithUserDetails
+    @DisplayName("프로젝트 조회")
+    void getProject() throws Exception {
+
+        Project project = Project.builder()
+                .member(TestDataConfig.member)
+                .name("테스트 프로젝트")
+                .cropType(CropType.팥)
+                .startDate(LocalDate.of(2022, 3, 1))
+                .endDate(LocalDate.of(2022, 12, 1))
+                .build();
+        projectRepository.save(project);
+
+        mockMvc.perform(get("/projects/{projectId}", project.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("success").value(true))
+                .andExpect(jsonPath("data.projectId").value(project.getId()))
+                .andExpect(jsonPath("data.name").value(project.getName()))
+                .andExpect(jsonPath("data.cropType").value(project.getCropType().name()))
+                .andExpect(jsonPath("data.startDate").value(project.getStartDate().toString()))
+                .andExpect(jsonPath("data.endDate").value(project.getEndDate().toString()))
+                .andExpect(jsonPath("data.completed").value(project.isCompleted()))
+                .andDo(document("get-project",
+                        pathParameters(
+                                parameterWithName("projectId").description("조회하려는 프로젝트 아이디")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").description("요청 성공 여부"),
+                                fieldWithPath("data").description("조회한 프로젝트 데이터"),
+                                fieldWithPath("data.projectId").description("프로젝트 아이디"),
+                                fieldWithPath("data.name").description("프로젝트 이름"),
+                                fieldWithPath("data.cropType").description("프로젝트에서 관리하는 작물 종류"),
+                                fieldWithPath("data.startDate").description("프로젝트에 설정한 시작 날짜"),
+                                fieldWithPath("data.endDate").description("프로젝트에 설정한 종료 날짜"),
+                                fieldWithPath("data.completed").description("프로젝트 완료 여부")
                         )
                 ));
     }
