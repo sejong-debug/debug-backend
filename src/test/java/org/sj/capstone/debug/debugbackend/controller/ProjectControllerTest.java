5079734eb721ed06a5a43df1f214810d7e42ad99
Cho-D-YoungRae <yrc9229@gmail.com>
2022-05-12 14:22:44 +0900

    TEST: Project Update API

--- src/test/java/org/sj/capstone/debug/debugbackend/controller/ProjectControllerTest.java
+++ src/test/java/org/sj/capstone/debug/debugbackend/controller/ProjectControllerTest.java
@@ -1,11 +1,13 @@
 package org.sj.capstone.debug.debugbackend.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.sj.capstone.debug.debugbackend.common.RestDocsConfig;
 import org.sj.capstone.debug.debugbackend.common.TestDataConfig;
 import org.sj.capstone.debug.debugbackend.dto.project.ProjectCreationDto;
+import org.sj.capstone.debug.debugbackend.dto.project.ProjectUpdateDto;
 import org.sj.capstone.debug.debugbackend.entity.CropType;
 import org.sj.capstone.debug.debugbackend.entity.Project;
 import org.sj.capstone.debug.debugbackend.repository.ProjectRepository;
@@ -26,6 +28,8 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -33,10 +37,10 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -155,6 +159,55 @@ class ProjectControllerTest {
                                 fieldWithPath("data.startDate").description("프로젝트에 설정한 시작 날짜"),
                                 fieldWithPath("data.endDate").description("프로젝트에 설정한 종료 날짜"),
                                 fieldWithPath("data.completed").description("프로젝트 완료 여부")
+                        )
+                ));
+    }
+
+    @Test
+    @WithUserDetails
+    @DisplayName("프로젝트 수정")
+    void updateProject() throws Exception {
+        Project project = Project.builder()
+                .member(TestDataConfig.member)
+                .name("테스트 프로젝트")
+                .cropType(CropType.팥)
+                .startDate(LocalDate.of(2022, 3, 1))
+                .endDate(LocalDate.of(2022, 12, 1))
+                .build();
+        projectRepository.save(project);
+
+        ProjectUpdateDto updateDto = new ProjectUpdateDto();
+        updateDto.setName("수정 프로젝트");
+        updateDto.setCropType(CropType.가지);
+        updateDto.setStartDate(null);
+        updateDto.setEndDate(null);
+        updateDto.setCompleted(false);
+
+        mockMvc.perform(put("/projects/{projectId}", project.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateDto)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("success").value(true))
+                .andExpect(jsonPath("data").value(project.getId()))
+                .andDo(document("update-project",
+                        pathParameters(
+                                parameterWithName("projectId").description("수정하려는 프로젝트 아이디")
+                        ),
+                        requestFields(
+                                fieldWithPath("name").description("프로젝트 이름"),
+                                fieldWithPath("cropType").description("프로젝트에서 관리하는 작물 종류"),
+                                fieldWithPath("startDate").description("프로젝트 시작 날짜"),
+                                fieldWithPath("endDate").description("프로젝트 종료 날짜"),
+                                fieldWithPath("completed").description("프로젝트 완료 여부")
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.LOCATION).description("수정된 프로젝트 조회 요청")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").description("요청 성공 여부"),
+                                fieldWithPath("data").description("수정된 프로젝트 아이디")
                         )
                 ));
     }
