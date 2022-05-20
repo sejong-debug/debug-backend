a5eb5c83f4b57ccb6dd8902a46ff137bfcc848a0
KangDaeJin <rkdeowls2580@naver.com>
2022-05-20 16:00:29 +0900

    FEAT : Statistics Controller 구현

--- src/main/java/org/sj/capstone/debug/debugbackend/controller/StatisticsController.java
+++ src/main/java/org/sj/capstone/debug/debugbackend/controller/StatisticsController.java
@@ -1,9 +1,15 @@
 package org.sj.capstone.debug.debugbackend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.sj.capstone.debug.debugbackend.dto.common.ApiResult;
+import org.sj.capstone.debug.debugbackend.dto.statistics.StatisticsDto;
+import org.sj.capstone.debug.debugbackend.entity.CropType;
 import org.sj.capstone.debug.debugbackend.service.StatisticsService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/statistics")
@@ -11,4 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
+
+    @GetMapping("/crop-types")
+    public ResponseEntity<ApiResult<StatisticsDto>> getStatisticsByCropType(
+            @RequestParam CropType cropType,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate) {
+        ApiResult<StatisticsDto> result = ApiResult.<StatisticsDto>builder()
+                .data(statisticsService.getDiseaseWithCropTypes(cropType, startDate, endDate))
+                .build();
+
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/projects/{projectId}")
+    public ResponseEntity<ApiResult<Integer>> getDiseaseCount(@PathVariable long projectId) {
+        ApiResult<Integer> result = ApiResult.<Integer>builder()
+                .data(statisticsService.getDiseaseCount(projectId))
+                .build();
+
+        return ResponseEntity.ok(result);
+    }
 }
